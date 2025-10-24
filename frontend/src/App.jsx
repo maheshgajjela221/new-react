f22bedc0b126b128f1070477716218531d2f3795
maheshgajjela221 <166375267+maheshgajjela221@users.noreply.github.com>
2025-10-24 14:19:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>mahesh visys</h1>
+      <h1>mahesh1 visys</h1>
       <Message text={msg} />
     </div>
   );
